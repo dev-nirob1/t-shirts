71ed503ac838035c39332f08bbb8bfb65a64a845
dev-nirob1 <dev-nirob1@gmail.com>
2023-04-27 23:34:38 +0600

    header complete

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav>
+        <nav className='space-x-3 px-5 h-14 flex items-center text-white justify-between  font-semibold bg-stone-500'>
+            <h2 className='text-4xl'>T-Shirts</h2>
+            <div className='space-x-5 '>
             <Link to="/">Home</Link>
             <Link to="/review">Order Review</Link>
             <Link to="/about">About</Link>
             <Link to="/contact">Contact</Link>
+            </div>
         </nav>
     );
 };
